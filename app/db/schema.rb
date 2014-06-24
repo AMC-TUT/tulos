2812c373c0e8972aaa03744f213fc0f78f46fedb
teemujonkkari <teemu.jonkkari@gmail.com>
2014-06-24 17:31:59 +0300

    user admin and teacher booleans

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140624120611) do
+ActiveRecord::Schema.define(version: 20140624141137) do
 
   create_table "answers", force: true do |t|
     t.integer  "denominator"
@@ -54,12 +54,12 @@ ActiveRecord::Schema.define(version: 20140624120611) do
   add_index "traps", ["level_id"], name: "index_traps_on_level_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 20140624120611) do
     t.string   "username"
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "teacher",                default: false
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
