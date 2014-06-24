8318fd752a76083307ff62525dd57e5fa532f7f4
teemujonkkari <teemu.jonkkari@gmail.com>
2014-06-24 21:48:06 +0300

    User index view

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -2,15 +2,19 @@ App::Application.routes.draw do
 
   resources :worlds
 
-  devise_for :users, path: "auth", path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    password: 'secret',
-    confirmation: 'verification',
-    unlock: 'unblock',
-    registration: 'register',
-    sign_up: 'cmon_let_me_in'
-  }
+
+  devise_for :users #,
+  #   path: "auth", path_names: {
+  #   sign_in: 'login',
+  #   sign_out: 'logout',
+  #   password: 'secret',
+  #   confirmation: 'verification',
+  #   unlock: 'unblock',
+  #   registration: 'register',
+  #   sign_up: 'cmon_let_me_in'
+  # }
+
+  resources :users
 
   root to: "worlds#index"
 
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140624141137) do
+ActiveRecord::Schema.define(version: 20140624154155) do
 
   create_table "answers", force: true do |t|
     t.integer  "denominator"
@@ -42,6 +42,24 @@ ActiveRecord::Schema.define(version: 20140624141137) do
   end
 
   add_index "numbers", ["level_id"], name: "index_numbers_on_level_id", using: :btree
+
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+
+  create_table "tags", force: true do |t|
+    t.string "name"
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "traps", force: true do |t|
     t.integer  "denominator"
