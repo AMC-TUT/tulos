f2163b732edb5051444ac9fcca50ab52aeb94d48
teemujonkkari <teemu.jonkkari@gmail.com>
2014-06-24 22:26:14 +0300

    User form view

--- app/app/models/world.rb
+++ app/app/models/world.rb
@@ -1,7 +1,7 @@
 class World < ActiveRecord::Base
   has_many :levels, :dependent => :destroy
 
-  accepts_nested_attributes_for :levels #, :reject_if => lambda { |l| l[:type].blank? }, :allow_destroy => true
+  accepts_nested_attributes_for :levels, :reject_if => lambda { |l| l[:type].blank? }, :allow_destroy => true
   validates_associated :levels
 
   validates :title, :presence => true, :length => { :minimum => 5, :maximum => 255 }
